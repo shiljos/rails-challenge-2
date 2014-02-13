bbd4fb65fe159a2d67aec027d968c65bccc18762
shiljos <shiljos@gmail.com>
2014-02-13 14:39:13 +0100

    rails challenge ver3

--- source/test_app/spec/factories.rb
+++ source/test_app/spec/factories.rb
@@ -5,6 +5,17 @@
 	  about "Let others speak for me."
 	  first_name "John"
 	  last_name "Markovich"
+
+	  factory :user_with_stories do
+
+	    ignore do
+	      story_count 5
+   	    end
+
+  	    after(:create) do |user, evaluator|
+  	      create_list(:story, evaluator.story_count, user: user )
+	    end
+	  end
     end
 
     factory :story do
@@ -13,6 +24,21 @@
       url "https://www.google.me"
       title "Titl"
       description "Some description"
+
+      factory :innactive_stories do
+      	active false
+      end
+
+      factory :stories_with_comments do
+
+        ignore do
+          comment_count 5
+        end
+
+        after(:create) do |story, evaluator|
+          create_list(:comment, evaluator.comment_count, story: story)
+        end
+      end
     end
 
     factory :comment do
--- source/test_app/spec/request/user_pages_spec.rb
+++ source/test_app/spec/request/user_pages_spec.rb
@@ -5,74 +5,21 @@ describe "User pages" do
   subject { page }
 
   before do
-    # @user1 =User.create!(username: "johny",
-    #  email: "johny@gmail.com",
-    #  about: "Let others speak for me.",
-    #  first_name: "John", 
-    #  last_name: "Markovich")
-    # @user2 = User.create!( username: "ben",
-    #  email: "ben@gmail.com",
-    #  about: "Ooh, I don't know what to say.",
-    #  first_name: "Ben", 
-    #  last_name: "Smith")
-    # @user3 = User.create!(username: "george",
-    #  email: "george@gmail.com",
-    #  about: "I am amazing",
-    #  first_name: "George", 
-    #  last_name: "Adams")
 
-    @user1 = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
 
-    3.times do |n|
-      # Story.create!(user: @user1, active: true, url: "https://trello.com", title: "Sample story user 1",
-      #   description: "some description") 
-        FactoryGirl.create(:story, user_id:1)
-    end
+    FactoryGirl.create_list(:story, 3, user: @user)
+    FactoryGirl.create_list(:innactive_stories, 2, user: @user)
+    FactoryGirl.create_list(:comment, 3, user: @user)
 
-    2.times do |n|
-      # Story.create!(user: @user2, active: true, url: "https://www.google.me", title: "Sample story 2",
-      #   description: "some description 2") 
-        FactoryGirl.create(:story, user_id:2)
-    end
-
-    2.times do |n|
-      # Story.create!(user: @user3, active: true, url: "https://www.google.me", title: "Sample story story",
-      #   description: "some description 3") 
-        FactoryGirl.create(:story, user_id:3)
-    end
-
-    # @in1 = Story.create!(user: @user1, active: false, url: "https://trello.com", title: "Sample story",
-    #   description: "some description")
-    2.times do
-      @in1 = FactoryGirl.create(:story, user_id:1, active:false)
-    end
-
-    2.times do |n|
-      # Story.create!(user: @user2, active: false, url: "https://www.google.me", title: "Sample story 2",
-      #   description: "some description 2") 
-        FactoryGirl.create(:story, user_id:2, active:false)
-    end
-
-    3.times do |n|
-      # Story.create!(user: @user3, active: false, url: "https://www.google.me", title: "Sample story 2",
-      #   description: "some description 2") 
-        FactoryGirl.create(:story, user_id:3, active:false)
-    end
-
-
-    3.times do |n|
-        FactoryGirl.create(:comment, story_id:1, user_id:1)
-     ## Comment.create!(story_id: 1, user: @user1, comment: "amazing story")
-      #Comment.create!(story_id: 2, user_id: rand(3)+1, comment: "amazing story 2")
-      #Comment.create!(story_id: 2, user_id: rand(3)+1, comment: "story 2 ")
-    end
-
-    visit user_path(@user1)
+    visit user_path(@user)
   end
 
-  it {should have_selector('h1', text:'Active stories')}
-  it {should have_selector('h1', text:'Inactive stories')}
-  it {should have_selector('h1', text:'Commented stories')}
+  describe "should have 3 lists" do
+    it {should have_selector('h1', text:'Active stories')}
+    it {should have_selector('h1', text:'Inactive stories')}
+    it {should have_selector('h1', text:'Commented stories')}
+  end
 
   describe "correct number of stories" do
     it {page.all("div.active li").count.should eql(9)}
