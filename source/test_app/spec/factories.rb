ceb482f67caa53a19c1747c7660804fb347e058d
shiljos <shiljos@gmail.com>
2014-02-13 11:32:09 +0100

    rails-challenge2-nv

--- source/test_app/spec/factories.rb
+++ source/test_app/spec/factories.rb
@@ -1,14 +1,23 @@
-FactoryGirl.define do
-	factory :user do
-	  username        "mich"
-      email           "mich@gmail.com"
-      about           "asdasdasd"
-      first_name      "Michael"
-      last_name       "Jordan"
+  FactoryGirl.define do
+  	factory :user do
+	  sequence(:username) { |n| "johny #{n}" }
+	  sequence(:email) { |n| "johny_#{n}@gmail.com" }
+	  about "Let others speak for me."
+	  first_name "John"
+	  last_name "Markovich"
+    end
 
-   
+    factory :story do
+      user
+      active true
+      url "https://www.google.me"
+      title "Titl"
+      description "Some description"
+    end
 
-    #factory :story do
-    	
-     end
-end
+    factory :comment do
+    	story
+    	user
+    	comment "This is an amazing story!"
+    end
+  end
--- source/test_app/spec/request/user_pages_spec.rb
+++ source/test_app/spec/request/user_pages_spec.rb
@@ -5,53 +5,64 @@ describe "User pages" do
   subject { page }
 
   before do
-    @user1 =User.create!(username: "johny",
-     email: "johny@gmail.com",
-     about: "Let others speak for me.",
-     first_name: "John", 
-     last_name: "Markovich")
-    @user2 = User.create!( username: "ben",
-     email: "ben@gmail.com",
-     about: "Ooh, I don't know what to say.",
-     first_name: "Ben", 
-     last_name: "Smith")
-    @user3 = User.create!(username: "george",
-     email: "george@gmail.com",
-     about: "I am amazing",
-     first_name: "George", 
-     last_name: "Adams")
+    # @user1 =User.create!(username: "johny",
+    #  email: "johny@gmail.com",
+    #  about: "Let others speak for me.",
+    #  first_name: "John", 
+    #  last_name: "Markovich")
+    # @user2 = User.create!( username: "ben",
+    #  email: "ben@gmail.com",
+    #  about: "Ooh, I don't know what to say.",
+    #  first_name: "Ben", 
+    #  last_name: "Smith")
+    # @user3 = User.create!(username: "george",
+    #  email: "george@gmail.com",
+    #  about: "I am amazing",
+    #  first_name: "George", 
+    #  last_name: "Adams")
+
+    @user1 = FactoryGirl.create(:user)
 
     3.times do |n|
-      Story.create!(user: @user1, active: true, url: "https://trello.com", title: "Sample story user 1",
-        description: "some description") 
+      # Story.create!(user: @user1, active: true, url: "https://trello.com", title: "Sample story user 1",
+      #   description: "some description") 
+        FactoryGirl.create(:story, user_id:1)
     end
 
     2.times do |n|
-      Story.create!(user: @user2, active: true, url: "https://www.google.me", title: "Sample story 2",
-        description: "some description 2") 
+      # Story.create!(user: @user2, active: true, url: "https://www.google.me", title: "Sample story 2",
+      #   description: "some description 2") 
+        FactoryGirl.create(:story, user_id:2)
     end
 
     2.times do |n|
-      Story.create!(user: @user3, active: true, url: "https://www.google.me", title: "Sample story story",
-        description: "some description 3") 
+      # Story.create!(user: @user3, active: true, url: "https://www.google.me", title: "Sample story story",
+      #   description: "some description 3") 
+        FactoryGirl.create(:story, user_id:3)
     end
 
-    @in1 = Story.create!(user: @user1, active: false, url: "https://trello.com", title: "Sample story",
-      description: "some description")
+    # @in1 = Story.create!(user: @user1, active: false, url: "https://trello.com", title: "Sample story",
+    #   description: "some description")
+    2.times do
+      @in1 = FactoryGirl.create(:story, user_id:1, active:false)
+    end
 
     2.times do |n|
-      Story.create!(user: @user2, active: false, url: "https://www.google.me", title: "Sample story 2",
-        description: "some description 2") 
+      # Story.create!(user: @user2, active: false, url: "https://www.google.me", title: "Sample story 2",
+      #   description: "some description 2") 
+        FactoryGirl.create(:story, user_id:2, active:false)
     end
 
     3.times do |n|
-      Story.create!(user: @user3, active: false, url: "https://www.google.me", title: "Sample story 2",
-        description: "some description 2") 
+      # Story.create!(user: @user3, active: false, url: "https://www.google.me", title: "Sample story 2",
+      #   description: "some description 2") 
+        FactoryGirl.create(:story, user_id:3, active:false)
     end
 
 
     3.times do |n|
-      Comment.create!(story_id: 1, user: @user1, comment: "amazing story")
+        FactoryGirl.create(:comment, story_id:1, user_id:1)
+     ## Comment.create!(story_id: 1, user: @user1, comment: "amazing story")
       #Comment.create!(story_id: 2, user_id: rand(3)+1, comment: "amazing story 2")
       #Comment.create!(story_id: 2, user_id: rand(3)+1, comment: "story 2 ")
     end
@@ -65,24 +76,24 @@ describe "User pages" do
 
   describe "correct number of stories" do
     it {page.all("div.active li").count.should eql(9)}
-    it {page.all("div.innactive li").count.should eql(4)}
+    it {page.all("div.innactive li").count.should eql(8)}
     it {page.all("div.commented li").count.should eql(9)}
   end
 
   describe "story activation" do
 
     before do
-      check('active'+ @in1.id.to_s)
       @innactive = Story.where(active: false)
       @active = Story.where(active: true)
+      check("mycheckbox-#{@innactive.first.id}")
     end
 
     it "should decrease number of innactive stories" do
-      expect { click_button "Activate story" }.to change(@innactive, :count).by(-1)
+      expect { click_button "Activate story #{@innactive.first.id}" }.to change(@innactive, :count).by(-1)
     end
 
     it "should increase number of active stories" do
-      expect { click_button "Activate story" }.to change(@active, :count).by(1)
+      expect { click_button "Activate story #{@innactive.first.id}" }.to change(@active, :count).by(1)
     end
   end
 end
